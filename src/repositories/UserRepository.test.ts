08ae9fa92d573d5700025fafcd3e9ccf24875a07
Natalia <natirgh@gmail.com>
2022-08-31 11:54:53 +0200

    tested error response in registration method

--- src/repositories/UserRepository.test.ts
+++ src/repositories/UserRepository.test.ts
@@ -9,13 +9,24 @@ describe("Given a UserRepository class", () => {
       expect(userRepository).toBeTruthy();
     });
 
-    test("And then the method registration should make a post request with de user data received", async () => {
+    describe("When the registration method is called", () => {
       const userData = { name: "", password: "", username: "" };
-      axios.post = jest.fn().mockResolvedValue({ data: userData });
+      test("Then if the API returns an OK response, the method should return the data of the response", async () => {
+        axios.post = jest.fn().mockResolvedValue({ data: userData });
 
-      const returnedValue = await userRepository.registration(userData);
+        const returnedValue = await userRepository.registration(userData);
 
-      expect(returnedValue).toStrictEqual(userData);
+        expect(returnedValue).toStrictEqual(userData);
+      });
+
+      test("And then if the API returns an error, the method should return an error", async () => {
+        const error = {};
+        axios.post = jest.fn().mockRejectedValue(error);
+
+        const returnedValue = await userRepository.registration(userData);
+
+        expect(returnedValue).toStrictEqual(error);
+      });
     });
   });
 });
