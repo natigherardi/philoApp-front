f4d834afd601773f43c5efbb209d56650612f33f
Natalia <natirgh@gmail.com>
2022-08-31 15:43:34 +0200

    installed styled components and created style object

---
+++ src/style/style.ts
@@ -0,0 +1,24 @@
+const style = {
+  fontFamily: {
+    primaryFont: "Lato, sans-serif",
+  },
+  colors: {
+    primaryColor: "FFFFFF",
+    secondaryColor: "D1D1D1",
+    contrastColor: "A23B72",
+    contrastColorLighter: "D475A8",
+    TypographyColor: "1F1F1F",
+  },
+  fontSize: {
+    xl: 62,
+    large: 52,
+    medium: 32,
+    small: 24,
+  },
+  borderRadius: {
+    large: 50,
+    medium: 16,
+  },
+};
+
+export default style;
