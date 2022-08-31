9229f56de60d4c6971766b8607169c3041512ce9
Natalia <natirgh@gmail.com>
2022-08-31 19:49:11 +0200

    added handle on change to inputs

--- src/components/FormUser/FormUser.tsx
+++ src/components/FormUser/FormUser.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import FormContainer from "./FormUserStyled";
 
 const FormUser = (): JSX.Element => {
+  const [userData, setUserData] = useState({
+    name: "",
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({ ...userData, [event.target.id]: event.target.value });
+  };
+
   return (
     <FormContainer>
       <form>
@@ -10,30 +21,36 @@ const FormUser = (): JSX.Element => {
           </label>
           <input
             type="text"
-            name="name"
+            id="name"
             required
             autoComplete="off"
             className="form__input"
+            value={userData.name}
+            onChange={handleChange}
           />
           <label htmlFor="username" className="form__label">
             Username
           </label>
           <input
             type="text"
-            name="username"
+            id="username"
             required
             autoComplete="off"
             className="form__input"
+            value={userData.username}
+            onChange={handleChange}
           />
           <label htmlFor="password" className="form__label">
             Password
           </label>
           <input
             type="password"
-            name="password"
+            id="password"
             required
             autoComplete="off"
             className="form__input"
+            value={userData.password}
+            onChange={handleChange}
           />
         </div>
         <button type="submit">Submit</button>
