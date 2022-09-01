c5b67e337b9e474aba11776b24060f2ac7311f72
Natalia <natirgh@gmail.com>
2022-09-01 16:14:44 +0200

    refactored api url property of user repository

--- src/repositories/UserRepository.test.ts
+++ src/repositories/UserRepository.test.ts
@@ -3,8 +3,7 @@ import UserRepository from "./UserRepository";
 
 describe("Given a UserRepository class", () => {
   describe("When we have an instance", () => {
-    const url = "";
-    const userRepository = new UserRepository(url);
+    const userRepository = new UserRepository();
     test("Then it should exist", () => {
       expect(userRepository).toBeTruthy();
     });
@@ -14,7 +13,7 @@ describe("Given a UserRepository class", () => {
       test("Then if the API returns an OK response, the method should return the data of the response", async () => {
         axios.post = jest.fn().mockResolvedValue({ data: userData });
 
-        const returnedValue = await userRepository.registration(userData);
+        const returnedValue = await UserRepository.registration(userData);
 
         expect(returnedValue).toStrictEqual(userData);
       });
@@ -23,7 +22,7 @@ describe("Given a UserRepository class", () => {
         const error = {};
         axios.post = jest.fn().mockRejectedValue(error);
 
-        const returnedValue = await userRepository.registration(userData);
+        const returnedValue = await UserRepository.registration(userData);
 
         expect(returnedValue).toStrictEqual(error);
       });
