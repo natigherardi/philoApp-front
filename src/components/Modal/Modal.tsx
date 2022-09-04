292a10ebd765c268469bd1c4185e8daa0e0164ac
Natalia <natirgh@gmail.com>
2022-09-04 23:46:17 +0200

    close modal icon and cond rend error/success

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,18 +1,44 @@
 import ModalStyled from "./ModalStyled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { useAppDispatch } from "../../store/hooks";
+import { closeModalActionCreator } from "../../store/ui/uiSlice";
 
 const Modal = (): JSX.Element => {
-  return (
-    <ModalStyled>
-      <FontAwesomeIcon icon={faX} className="modal__close-icon" />
-      <span className="modal__title">Success ✓</span>
+  const {
+    modal: { isError, isOpen, message },
+  } = useSelector((state: RootState) => state.ui);
 
-      <i></i>
-      <span className="modal__description">
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-      </span>
-    </ModalStyled>
+  const dispatch = useAppDispatch();
+
+  const text = isError ? "Error ꭗ" : "Success ✓";
+  if (isOpen) {
+    setTimeout(() => {
+      dispatch(closeModalActionCreator());
+    }, 5000);
+  }
+
+  const handleClose = () => {
+    dispatch(closeModalActionCreator());
+  };
+  return (
+    <>
+      {isOpen && (
+        <ModalStyled>
+          <FontAwesomeIcon
+            icon={faX}
+            className="modal__close-icon"
+            onClick={handleClose}
+          />
+          <span className="modal__title">{text}</span>
+          <span className="modal__description">{message}</span>
+        </ModalStyled>
+      )}
+
+      {!isOpen && <></>}
+    </>
   );
 };
 
