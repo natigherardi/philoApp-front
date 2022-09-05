1c5ada4bd189fa2a10070a22e7825223e36374c6
Natalia <natirgh@gmail.com>
2022-09-05 16:40:56 +0200

    added route to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import MainContainer from "./AppStyled";
 import { Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to={"/register"} />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </MainContainer>
   );
