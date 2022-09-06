4a1771abcc0317e797bdce399adb6953e5187f0f
Natalia <natirgh@gmail.com>
2022-09-06 11:37:05 +0200

    fixed margin and added is loading modal

--- src/AppStyled.tsx
+++ src/AppStyled.tsx
@@ -2,7 +2,9 @@ import docStyle from "./style/style";
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  margin: ${docStyle.padding.medium};
+  padding: ${docStyle.padding.medium};
+  width: 100vw;
+  height: 100vh;
 `;
 
 export default MainContainer;
