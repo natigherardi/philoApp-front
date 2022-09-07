c1135ed05beb810403b69ee49553f2d343b0571a
Natalia <natirgh@gmail.com>
2022-09-07 21:36:07 +0200

    structure, style and functionality to quotes list

--- src/components/QuotesList/QuotesList.tsx
+++ src/components/QuotesList/QuotesList.tsx
@@ -1,18 +1,19 @@
 import { useAppSelector } from "../../store/hooks";
 import Quote from "../../types/Quote";
 import QuoteCard from "../QuoteCard/QuoteCard";
+import QuotesListStyled from "./QuotesListStyled";
 
 const QuotesList = (): JSX.Element => {
   const quotesCards: Quote[] = useAppSelector((state) => state.quotes);
 
   return (
-    <ul>
+    <QuotesListStyled>
       {quotesCards.map((quote) => (
         <li key={quote.id}>
           <QuoteCard quote={quote} />
         </li>
       ))}
-    </ul>
+    </QuotesListStyled>
   );
 };
 
---
+++ src/components/QuotesList/QuotesListStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const QuotesListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+`;
+
+export default QuotesListStyled;
