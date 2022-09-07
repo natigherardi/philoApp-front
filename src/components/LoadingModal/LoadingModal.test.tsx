0e9d1fc39279e4544bffb578d2e6473d5525641f
Natalia <natirgh@gmail.com>
2022-09-07 14:13:49 +0200

    tested loading modal

---
+++ src/components/LoadingModal/LoadingModal.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import App from "../../App";
+import Wrapper from "../../testUtils/Wrapper";
+import LoadingModal from "./LoadingModal";
+
+let mockedUseSelector = {};
+let mockedDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: () => mockedUseSelector,
+  useDispatch: () => mockedDispatch,
+}));
+
+describe("Guiven a loading modal", () => {
+  describe("When it's rendered and the page is loading", () => {
+    test("Then it should show a spinner and a test 'Loading...", () => {
+      mockedUseSelector = {
+        modal: {},
+        isLoading: true,
+      };
+
+      render(
+        <Wrapper>
+          <App />
+        </Wrapper>
+      );
+
+      const loadingText = screen.getByText("Loading...");
+
+      expect(loadingText).toBeInTheDocument();
+    });
+  });
+});
