761c1b5528877d27303e194516941f00f66cd31b
Natalia <natirgh@gmail.com>
2022-09-08 10:20:41 +0200

    fixed image size and render list in home

--- src/components/QuoteCard/QuoteCardStyled.ts
+++ src/components/QuoteCard/QuoteCardStyled.ts
@@ -7,7 +7,6 @@ const QuoteCardStyled = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: ${docStyle.borderRadius.medium};
-  gap: 20px;
   .card {
     &__text {
       font-size: ${docStyle.fontSize.medium};
@@ -15,13 +14,23 @@ const QuoteCardStyled = styled.div`
     }
     &__author {
       font-size: ${docStyle.fontSize.small};
+      font-size: 24px;
+      margin-top: 23px;
     }
     &__image {
-      width: 165px;
-      height: 150px;
+      width: auto;
       overflow: hidden;
-      border-radius: ${docStyle.borderRadius.large};
-      align-self: end;
+      border-radius: 50%;
+      object-fit: cover;
+      height: 150px;
+      width: 150px;
+      &-container {
+        width: 150px px;
+        height: 150px;
+        display: flex;
+        align-self: flex-end;
+        border-radius: 50%;
+      }
     }
   }
 `;
