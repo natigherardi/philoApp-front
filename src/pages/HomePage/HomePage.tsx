2c3d04cdc4bda5169e0016467f673e30391ae2c5
Natalia <natirgh@gmail.com>
2022-09-08 10:27:22 +0200

    deleted unused import

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,3 @@
-import QuoteCard from "../../components/QuoteCard/QuoteCard";
 import Header from "../../components/Header/Header";
 import QuotesList from "../../components/QuotesList/QuotesList";
 
