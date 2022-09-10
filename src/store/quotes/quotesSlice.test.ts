7381c9ddf596b9622a8f5460a9c62d766ea174c6
Natalia <natirgh@gmail.com>
2022-09-10 14:43:27 +0200

    added case reducer to delete quote from store

--- src/store/quotes/quotesSlice.test.ts
+++ src/store/quotes/quotesSlice.test.ts
@@ -1,17 +1,31 @@
 import { QuotesStore } from "../../types/Quote";
 import {
+  deleteQuoteActionCreator,
   loadPrivateQuotesActionCreator,
   loadPublicQuotesActionCreator,
   quotesReducer,
 } from "./quotesSlice";
 
 describe("Given a quotes reducer", () => {
-  const previusQuotes: QuotesStore = { privateQuotes: [], publicQuotes: [] };
+  const previusQuotes: QuotesStore = {
+    privateQuotes: [
+      {
+        author: "test previus quote",
+        image: "test previus quote",
+        owner: "test previus quote",
+        id: "test previus quote",
+        textContent: "test previus quote",
+        user: "test previus quote",
+      },
+    ],
+    publicQuotes: [],
+  };
   const loadedQuotes = [
     {
       author: "test",
       image: "test",
       owner: "test",
+      id: "test",
       textContent: "test",
       user: "test",
     },
@@ -35,6 +49,22 @@ describe("Given a quotes reducer", () => {
       );
 
       expect(newQuotes.privateQuotes).toStrictEqual(loadedQuotes);
+    });
+  });
+
+  describe("When it is called with a deleteQuote action with a quote id as payload", () => {
+    test("Then it should return the list of private quotes without the quote received", () => {
+      const quoteToDeleteId = "test previus quote";
+
+      const { privateQuotes } = quotesReducer(
+        previusQuotes,
+        deleteQuoteActionCreator(quoteToDeleteId)
+      );
+      const resultDelete = previusQuotes.privateQuotes.filter(
+        (quote) => quote.id !== quoteToDeleteId
+      );
+
+      expect(privateQuotes).toStrictEqual(resultDelete);
     });
   });
 });
