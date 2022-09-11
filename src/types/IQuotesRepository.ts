50331d365f5a4393258a6a612556a17d0feaf632
Natalia <natirgh@gmail.com>
2022-09-11 21:01:02 +0200

    create quote method in repository and interface

--- src/types/IQuotesRepository.ts
+++ src/types/IQuotesRepository.ts
@@ -3,4 +3,5 @@ import { Item } from "./IUserRepository";
 export interface IQuotesRepository<T extends Item> {
   getAllQuotes: () => Promise<T[]>;
   getQuotesByUser: (token: string, id: string) => Promise<T>;
+  createQuote: (quote: FormData, token: string, userId: string) => Promise<T>;
 }
