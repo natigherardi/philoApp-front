c2908eb65d2143858ba4005f3e77621aa7171171
Natalia <natirgh@gmail.com>
2022-09-12 13:04:27 +0200

    deleted form from home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,10 @@
 import Header from "../../components/Header/Header";
-import QuoteForm from "../../components/QuoteForm/QuoteForm";
 import QuotesList from "../../components/QuotesList/QuotesList";
 
 const HomePage = (): JSX.Element => {
   return (
     <>
       <Header />
-      <QuoteForm></QuoteForm>
       <QuotesList />
     </>
   );
