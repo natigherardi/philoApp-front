5995137caf254ba4f2ed4e98142b0bc7616c2b12
Natalia <natirgh@gmail.com>
2022-09-12 18:25:21 +0200

    added method get quote by id to quotes repo interface

--- src/types/IQuotesRepository.ts
+++ src/types/IQuotesRepository.ts
@@ -4,4 +4,5 @@ export interface IQuotesRepository<T extends Item> {
   getAllQuotes: () => Promise<T[]>;
   getQuotesByUser: (token: string, id: string) => Promise<T>;
   createQuote: (quote: FormData, token: string, userId: string) => Promise<T>;
+  getQuoteById: (quoteId: string) => Promise<T>;
 }
