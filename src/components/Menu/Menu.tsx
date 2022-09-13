ac8f08c89ed5bf93fb481e11d25736d72f997ad8
Natalia <natirgh@gmail.com>
2022-09-13 10:13:11 +0200

    added span to menu to log out with function

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,31 @@
 import { slide as BurgerMenu } from "react-burger-menu";
 import { NavLink } from "react-router-dom";
+import useQuotes from "../../hooks/useQuotes/useQuotes";
+import { useUserSession } from "../../hooks/useUserSession/useUserSession";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { logoutUserActionCreator } from "../../store/user/userSessionSlice";
 import "./menu.css";
 
 const Menu = (): JSX.Element => {
+  const { isLoggedIn } = useAppSelector((state) => state.userSession);
+  const dispatch = useAppDispatch();
+
+  const handleLogOut = () => {
+    dispatch(logoutUserActionCreator());
+  };
+
   return (
     <BurgerMenu right noOverlay>
       <NavLink to={"/home"}>Home</NavLink>
-      <NavLink to={"/register"}>Register</NavLink>
-      <NavLink to={"/login"}>Log In</NavLink>
       <NavLink to={"/my-quotes"}>MyQuotes</NavLink>
       <NavLink to={"/create-quote"}>Create a quote</NavLink>
+      {!isLoggedIn && (
+        <>
+          <NavLink to={"/register"}>Register</NavLink>
+          <NavLink to={"/login"}>Log In</NavLink>
+        </>
+      )}
+      <span onClick={handleLogOut}>Log Out</span>
     </BurgerMenu>
   );
 };
