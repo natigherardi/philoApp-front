7ac46b6b51d9f8e46ca9c96d2d159f4411d7cffa
Natalia <natirgh@gmail.com>
2022-09-13 02:52:03 +0200

    tested useNavigate in quote card

--- src/components/QuoteCard/QuoteCard.test.tsx
+++ src/components/QuoteCard/QuoteCard.test.tsx
@@ -8,8 +8,14 @@ jest.mock("../../hooks/useQuotes/useQuotes", () => () => ({
   deleteQuote: mockDelete,
 }));
 
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
 describe("Given a Quote Card component", () => {
-  const mockQuoteId = "mock id";
+  const mockQuoteId = "mock-id";
   const quote = {
     textContent: "test text",
     author: "test",
@@ -17,7 +23,7 @@ describe("Given a Quote Card component", () => {
     image: "text image",
     id: mockQuoteId,
   };
-  describe("When it receives a quote object and it's rendered in the public quotes page", () => {
+  describe("When it receives a quote object", () => {
     test("Then it should show a paragraph with the text received", () => {
       render(
         <Wrapper>
@@ -33,21 +39,38 @@ describe("Given a Quote Card component", () => {
         expect(element).toBeInTheDocument();
       });
     });
-  });
 
-  describe("And when it's rendered in private quotes page", () => {
-    test("Then it should also show a button which when clicked should  call the delete quote function with the quote id", async () => {
+    test("And then it should also show a detail button that when clicked navigate function is called with 'quote' + the quote ID", async () => {
+      const expectedTo = "/quote/mock-id";
+
       render(
         <Wrapper>
           <QuoteCard quote={quote} />
         </Wrapper>
       );
 
-      const button = screen.getAllByRole("button")[1];
+      const button = screen.getAllByRole("button")[0];
       await userEvent.click(button);
 
       await waitFor(() => {
-        expect(mockDelete).toHaveBeenCalledWith(mockQuoteId);
+        expect(mockNavigate).toHaveBeenCalledWith(expectedTo);
+      });
+    });
+
+    describe("And when it's rendered in private quotes page", () => {
+      test("Then it should also show a button which when clicked should  call the delete quote function with the quote id", async () => {
+        render(
+          <Wrapper>
+            <QuoteCard quote={quote} />
+          </Wrapper>
+        );
+
+        const button = screen.getAllByRole("button")[1];
+        await userEvent.click(button);
+
+        await waitFor(() => {
+          expect(mockDelete).toHaveBeenCalledWith(mockQuoteId);
+        });
       });
     });
   });
