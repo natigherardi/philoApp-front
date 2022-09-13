30d92dbd7b8bdeada69cadbcc02c2a46532f99ab
Natalia <natirgh@gmail.com>
2022-09-13 19:27:32 +0200

    refactored home & myquotes pages tests mocking useQuotes

--- src/pages/MyQuotesPage/MyQuotesPage.test.tsx
+++ src/pages/MyQuotesPage/MyQuotesPage.test.tsx
@@ -2,6 +2,10 @@ import { render, screen } from "@testing-library/react";
 import Wrapper from "../../testUtils/Wrapper";
 import MyQuotesPage from "./MyQuotesPage";
 
+jest.mock("../../hooks/useQuotes/useQuotes", () => () => ({
+  loadPrivateQuotes: () => jest.fn().mockReturnValue([]),
+}));
+
 describe("Given the MyQuotes page", () => {
   describe("When rendered", () => {
     test("Then it should show a header, a title and a quotes list", () => {
