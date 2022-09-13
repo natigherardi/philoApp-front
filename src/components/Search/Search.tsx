30d6268b523188500361f08a474f7f6ccc6576b0
Natalia <natirgh@gmail.com>
2022-09-13 21:52:09 +0200

    added functionality to Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import listOfAuthors from "../../utils/listOfAuthors";
 import SearchStyled from "./SearchStyled";
 
 const Search = (): JSX.Element => {
+  const navigate = useNavigate();
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    navigate(`/quote-filter/${event.target.value}`);
+  };
+
   return (
-    <SearchStyled id="author">
+    <SearchStyled id="author" onChange={handleSelect}>
       <option hidden>Filter by</option>
       {listOfAuthors.map((author) => (
         <option value={author}>{author}</option>
