d4f8525adf181fb93b7dbcf31c548436a8ab7855
Natalia <natirgh@gmail.com>
2022-09-13 18:39:56 +0200

    fixed list test & refactored myquotespage to pass quotes to list

--- src/pages/MyQuotesPage/MyQuotesPage.tsx
+++ src/pages/MyQuotesPage/MyQuotesPage.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from "react";
 import Header from "../../components/Header/Header";
 import QuotesList from "../../components/QuotesList/QuotesList";
+import useQuotes from "../../hooks/useQuotes/useQuotes";
+import { useAppSelector } from "../../store/hooks";
 
 const MyQuotesPage = (): JSX.Element => {
+  const { privateQuotes } = useAppSelector((state) => state.quotes);
+  const { loadPrivateQuotes } = useQuotes();
+  const { id, token } = useAppSelector((state) => state.userSession.userData);
+
+  useEffect(() => {
+    (async () => {
+      await loadPrivateQuotes(token, id);
+    })();
+  }, [loadPrivateQuotes, id, token]);
+
   return (
     <>
       <Header />
       <h1>My Quotes</h1>
-      <QuotesList />
+      <QuotesList quotesToRender={privateQuotes} />
     </>
   );
 };
